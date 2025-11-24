4af064715633152db4d28a62c666363267f0226b
Lukas <lradlspo@htl-steyr.ac.at>
2025-11-24 14:06:19 +0100

    deleted module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rockpaperscissors {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rockpaperscissors to javafx.fxml;
-    exports com.example.rockpaperscissors;
-}
