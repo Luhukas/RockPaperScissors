2acd078aa8e4db98d473ea5a810af2ce2fde9290
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2025-11-26 14:42:19 +0100

    added fx:controller

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
@@ -1,14 +1,22 @@
 package com.example.rockpaperscissors;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 public class RockPaperScissorsController {
+    public Label scoreLabel;
+    public Label resultLabel;
+    public ProgressBar progressBar;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onClick(ActionEvent actionEvent) {
     }
 }
