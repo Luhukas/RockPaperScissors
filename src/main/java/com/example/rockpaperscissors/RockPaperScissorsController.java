573c5993c3585de6558b49de10399a3a4d55761e
Lukas <lradlspo@htl-steyr.ac.at>
2025-11-26 14:25:30 +0100

    .

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
@@ -3,7 +3,7 @@ package com.example.rockpaperscissors;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RockPaperScissorsController {
     @FXML
     private Label welcomeText;
 
