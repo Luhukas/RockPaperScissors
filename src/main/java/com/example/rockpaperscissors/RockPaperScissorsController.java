fd9b064cefaf65ca5d06ee8bd64fccddb5684477
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2025-12-03 13:54:49 +0100

    rebased frontend onto main

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
@@ -1,19 +1,36 @@
 package com.example.rockpaperscissors;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 
+import java.net.URL;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.Random;
 
-public class RockPaperScissorsController {
+public class RockPaperScissorsController implements Initializable {
+
     public Label scoreLabel;
     public Label highScoreLabel;
     public ProgressBar progressBar;
     public ListView<String> historyListView;
 
+    public Button rock;
+    public Button paper;
+    public Button scissor;
     private int score = 0;
     private int highScore = 0;
     private final String[] options = {"Rock", "Paper", "Scissors"};
@@ -38,6 +55,28 @@ public class RockPaperScissorsController {
         );
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        setButtonImage(rock, "rock.jpeg");
+        setButtonImage(paper, "paper.jpeg");
+        setButtonImage(scissor, "scissors.jpeg");
+    }
+
+    private void setButtonImage(Button btn, String imageName) {
+        Image img = new Image(
+                Objects.requireNonNull(
+                        getClass().getResource("/assets/images/" + imageName)
+                ).toString()
+        );
+
+        ImageView iv = new ImageView(img);
+        iv.setFitWidth(100);
+        iv.setFitHeight(100);
+        iv.setPreserveRatio(true);
+
+        btn.setGraphic(iv);
+    }
     private String calcRoundStatus(int userChoice, int botChoice) {
         String status;
 
@@ -47,8 +86,8 @@ public class RockPaperScissorsController {
 
         if (
                 (userChoice == 0 && botChoice == 2) ||
-                (userChoice == 1 && botChoice == 0) ||
-                (userChoice == 2 && botChoice == 1))
+                        (userChoice == 1 && botChoice == 0) ||
+                        (userChoice == 2 && botChoice == 1))
         {
             status = "Win";
         } else if (userChoice == botChoice) {
