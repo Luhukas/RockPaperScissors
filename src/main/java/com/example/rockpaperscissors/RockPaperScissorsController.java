303a2dcf618ccf830138bf651b5ff790de646274
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2025-12-03 14:55:07 +0100

    added Maven Dependency, MediaPlayer

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
@@ -2,7 +2,6 @@ package com.example.rockpaperscissors;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -10,6 +9,8 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import java.net.URL;
 import java.util.Objects;
@@ -26,15 +27,24 @@ public class RockPaperScissorsController implements Initializable {
     public Button paper;
     public Button scissor;
 
-    public void onClick(ActionEvent actionEvent) {
-    }
+    public Button mediaplayer;
+    private MediaPlayer player;
+    boolean isSoundPlaying = false;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         setButtonImage(rock, "rock.jpeg");
         setButtonImage(paper, "paper.jpeg");
         setButtonImage(scissor, "scissors.jpeg");
+        setButtonImage(mediaplayer, "vinyl.gif");
+
+        Media media = new Media(
+                Objects.requireNonNull(
+                        getClass().getResource("/assets/sound/sound.mp3")
+                ).toString()
+        );
+
+        player = new MediaPlayer(media);
     }
 
     private void setButtonImage(Button btn, String imageName) {
@@ -50,5 +60,18 @@ public class RockPaperScissorsController implements Initializable {
         iv.setPreserveRatio(true);
 
         btn.setGraphic(iv);
+    }
+
+    public void onClickMediaPlayer(ActionEvent actionEvent) {
+        if (isSoundPlaying){
+            player.pause();
+            isSoundPlaying = false;
+        } else{
+            player.play();
+            isSoundPlaying = true;
+        }
+    }
+
+    public void onClick(ActionEvent actionEvent) {
     }
 }
