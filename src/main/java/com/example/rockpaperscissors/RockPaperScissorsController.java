d67f30713a5a76b283a6babaf126f4f29380236c
Ganerdene Erdenebayar <gerdeneb@htl-steyr.ac.at>
2025-12-03 13:34:41 +0100

    changed design

--- src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
+++ src/main/java/com/example/rockpaperscissors/RockPaperScissorsController.java
@@ -1,22 +1,60 @@
 package com.example.rockpaperscissors;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-public class RockPaperScissorsController {
+import java.net.URL;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class RockPaperScissorsController implements Initializable {
     public Label scoreLabel;
     public Label resultLabel;
     public ProgressBar progressBar;
-    @FXML
-    private Label welcomeText;
+    public TableView historyTableView;
+    public TableColumn userColumnTableView;
+    public TableColumn statusColumnTableView;
+    public TableColumn botColumnTableView;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private final Map<String, String> imageMap = Map.of(
+            "0", "rock.jpeg",
+            "1", "paper.jpeg",
+            "2", "scissors.jpeg"
+    );
 
     public void onClick(ActionEvent actionEvent) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        AnchorPane root = (AnchorPane) scoreLabel.getParent();
+
+        for (Node node : root.getChildren()) {
+
+            if (node instanceof Button btn && btn.getId() != null) {
+
+                String imageName = imageMap.get(btn.getId());
+                if (imageName == null) continue;
+
+                Image image = new Image(Objects.requireNonNull(
+                        getClass().getResourceAsStream("/assets/images/" + imageName)
+                ));
+
+                ImageView iv = new ImageView(image);
+                iv.setFitWidth(100);
+                iv.setFitHeight(100);
+                iv.setPreserveRatio(true);
+
+                btn.setGraphic(iv);
+            }
+        }
     }
 }
